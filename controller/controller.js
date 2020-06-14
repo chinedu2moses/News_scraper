7fad677568627d0add9670eb35e4c5b4b8efa80e
chinedu moses <chinedu2moses@gmail.com>
2020-06-14 18:20:53 -0500

    end point scrape completed

---
+++ controller/controller.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var router = express.Router();
+var path = require("path");
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+var Comment = require("../models/Comment.js");
+var Article = require("../models/Article.js");
+
+router.get("/", function(req, res){
+    res.redirect("/articles");
+});
+
+router.get("/scrape", function (req, res) {
+    request("http://www.theverge.com", function (error, response, html) {
+        var $ = cheerio.load(html);
+        var titlesArray = [];
+
+        $(".c-entry-box--compact_title").each(function(i, element){
+            var result{};
+
+            result.title = $(this)
+            .children("a")
+            .text();
+            result.link = $(this)
+            .children("a")
+            .attr("href");
+
+            if(result.title !== "" && result.link !==""){
+            
+            if (titlesArray.indexOf(result.title)== -1) {
+                titlesArray.push(result.title);
+
+                Article.count({ title: result.title }, function(err, test){
+                    if (test === 0) {
+                        var entry = new Article(result);
+
+                        entry.save(function(err, doc){
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log(doc);
+                            }
+                        })
+                    }
+                })
+
+            } else {
+                console.log("Article already exist.");
+            } 
+                
+            } else {
+                console.log("Not saved to DB, missing data");
+            }
+        });
+        res.redirect("/");
+    });
+});
